92bb3a418c3d6a3def4632764da78232c89d87e4
admin <help@shazamlabs.com>
2026-03-03 14:37:29 +1300

    release: 1.3.6

---
+++ intellij-plugin/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "intellij-gherkin-plugin"
---
+++ intellij-plugin/src/main/java/com/qualimetry/intellij/gherkin/GherkinFileType.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2026 SHAZAM Analytics Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qualimetry.intellij.gherkin;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public final class GherkinFileType extends LanguageFileType {
+
+    public static final GherkinFileType INSTANCE = new GherkinFileType();
+
+    private GherkinFileType() {
+        super(GherkinLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Gherkin";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Cucumber Gherkin feature file";
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "feature";
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return null;
+    }
+}
